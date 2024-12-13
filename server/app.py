d3028704260c53a490134580397f58c53de68e4a
Kamal Mohamed <kamal.mohamed@msa.edu.eg>
2024-12-13 23:01:20 +0200

    deployment changes

--- server/app.py
+++ server/app.py
@@ -8,7 +8,8 @@ logger = logging.getLogger("AESLogger")
 
 app = Flask(__name__)
 origins = ["http://localhost:5173", "https://aes-project.vercel.app"]
-CORS(app, supports_credentials=True,resources={r"/*": {"origins": origins}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": origins}})
+
 
 def sub_bytes(s):
     for i in range(4):
@@ -33,11 +34,11 @@ def inv_shift_rows(s):
     s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
     s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
 
+
 def add_round_key(s, k):
     for i in range(4):
         for j in range(4):
             s[i][j] ^= k[i][j]
-
 
 
 xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
@@ -70,27 +71,58 @@ def inv_mix_columns(s):
 
 
 r_con = (
-    0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
-    0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D, 0x9A,
-    0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A,
-    0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39,
+    0x00,
+    0x01,
+    0x02,
+    0x04,
+    0x08,
+    0x10,
+    0x20,
+    0x40,
+    0x80,
+    0x1B,
+    0x36,
+    0x6C,
+    0xD8,
+    0xAB,
+    0x4D,
+    0x9A,
+    0x2F,
+    0x5E,
+    0xBC,
+    0x63,
+    0xC6,
+    0x97,
+    0x35,
+    0x6A,
+    0xD4,
+    0xB3,
+    0x7D,
+    0xFA,
+    0xEF,
+    0xC5,
+    0x91,
+    0x39,
 )
 
 
 def bytes2matrix(text):
-    """ Converts a 16-byte array into a 4x4 matrix.  """
-    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+    """Converts a 16-byte array into a 4x4 matrix."""
+    return [list(text[i : i + 4]) for i in range(0, len(text), 4)]
+
 
 def matrix2bytes(matrix):
-    """ Converts a 4x4 matrix into a 16-byte array.  """
+    """Converts a 4x4 matrix into a 16-byte array."""
     return bytes(sum(matrix, []))
 
+
 def xor_bytes(a, b):
-    """ Returns a new byte array with the elements xor'ed. """
-    return bytes(i^j for i, j in zip(a, b))
+    """Returns a new byte array with the elements xor'ed."""
+    return bytes(i ^ j for i, j in zip(a, b))
+
 
 def inc_bytes(a):
-    """ Returns a new byte array with the value increment by 1 """
+    """Returns a new byte array with the value increment by 1"""
     out = list(a)
     for i in reversed(range(len(out))):
         if out[i] == 0xFF:
@@ -99,6 +131,7 @@ def inc_bytes(a):
             out[i] += 1
             break
     return bytes(out)
+
 
 def pad(plaintext):
     """
@@ -109,6 +142,7 @@ def pad(plaintext):
     padding_len = 16 - (len(plaintext) % 16)
     padding = bytes([padding_len] * padding_len)
     return plaintext + padding
+
 
 def unpad(plaintext):
     """
@@ -121,16 +155,19 @@ def unpad(plaintext):
     assert all(p == padding_len for p in padding)
     return message
 
+
 def split_blocks(message, block_size=16, require_padding=True):
-        assert len(message) % block_size == 0 or not require_padding
-        return [message[i:i+16] for i in range(0, len(message), block_size)]
+    assert len(message) % block_size == 0 or not require_padding
+    return [message[i : i + 16] for i in range(0, len(message), block_size)]
 
 
 class AES:
     """
     Class for AES-128 encryption with CBC mode and PKCS#7 padding.
     """
+
     rounds_by_key_size = {16: 10, 24: 12, 32: 14}
+
     def __init__(self, master_key, disabled_steps=None):
         """
         Initializes the object with a given key.
@@ -139,12 +176,13 @@ class AES:
         self.n_rounds = AES.rounds_by_key_size[len(master_key)]
         self._key_matrices = self._expand_key(master_key)
         self.logs = []
-        self.disabled_steps =disabled_steps or []
+        self.disabled_steps = disabled_steps or []
 
-        
     def log_state(self, step, state):
         # Format the state as a hexadecimal string for logging
-        formatted_state = '\n'.join([' '.join(f'{byte:02x}' for byte in row) for row in state])
+        formatted_state = "\n".join(
+            [" ".join(f"{byte:02x}" for byte in row) for row in state]
+        )
         log_entry = {"step": step, "state": formatted_state}
         self.logs.append(log_entry)
 
@@ -182,7 +220,7 @@ class AES:
             key_columns.append(word)
 
         # Group key words in 4x4 byte matrices.
-        return [key_columns[4*i : 4*(i+1)] for i in range(len(key_columns) // 4)]
+        return [key_columns[4 * i : 4 * (i + 1)] for i in range(len(key_columns) // 4)]
 
     def encrypt_block(self, plaintext):
         print(plaintext)
@@ -242,22 +280,31 @@ class AES:
 
         for i in range(self.n_rounds - 1, 0, -1):
             add_round_key(cipher_state, self._key_matrices[i])
-            self.log_state(f"After AddRoundKey (Round {self.n_rounds - i})", cipher_state)
+            self.log_state(
+                f"After AddRoundKey (Round {self.n_rounds - i})", cipher_state
+            )
 
             inv_mix_columns(cipher_state)
-            self.log_state(f"After InvMixColumns (Round {self.n_rounds - i})", cipher_state)
+            self.log_state(
+                f"After InvMixColumns (Round {self.n_rounds - i})", cipher_state
+            )
 
             inv_shift_rows(cipher_state)
-            self.log_state(f"After InvShiftRows (Round {self.n_rounds - i})", cipher_state)
+            self.log_state(
+                f"After InvShiftRows (Round {self.n_rounds - i})", cipher_state
+            )
 
             inv_sub_bytes(cipher_state)
-            self.log_state(f"After InvSubBytes (Round {self.n_rounds - i})", cipher_state)
+            self.log_state(
+                f"After InvSubBytes (Round {self.n_rounds - i})", cipher_state
+            )
 
         add_round_key(cipher_state, self._key_matrices[0])
         self.log_state("After AddRoundKey (Final Round)", cipher_state)
 
         return matrix2bytes(cipher_state)
-    
+
+
 def parse_input(data, key_field, text_field):
     def to_bytes(input_data):
         if isinstance(input_data, str):
@@ -274,54 +321,58 @@ def parse_input(data, key_field, text_field):
     return key, text
 
 
-@app.route('/encrypt', methods=['POST'])
+@app.route("/encrypt", methods=["POST"])
 def encrypt():
-    try: 
+    try:
         data = request.get_json()
-        key, plaintext = parse_input(data, 'key', 'plaintext')
+        key, plaintext = parse_input(data, "key", "plaintext")
 
-        if(len(plaintext) % 16 != 0):
+        if len(plaintext) % 16 != 0:
             plaintext = pad(plaintext)
-        if(len(key) % 16 != 0):
+        if len(key) % 16 != 0:
             key = pad(key)
-        disabled_steps = data.get('disabled_steps', [])
+        disabled_steps = data.get("disabled_steps", [])
         print(disabled_steps)
-        
+
         if len(key) not in [16, 24, 32]:
             return jsonify({"message": f"Invalid key length: {len(key)} bytes"}), 400
-        
-        if(len(plaintext) % 16 != 0):
-            return jsonify({"message": "Invalid plaintext length: must be a multiple of 16 bytes"}), 400
 
-        aes = AES(key,disabled_steps=disabled_steps)
+        if len(plaintext) % 16 != 0:
+            return (
+                jsonify(
+                    {
+                        "message": "Invalid plaintext length: must be a multiple of 16 bytes"
+                    }
+                ),
+                400,
+            )
+
+        aes = AES(key, disabled_steps=disabled_steps)
         ciphertext = aes.encrypt_block(plaintext)
 
-        return jsonify({
-            "ciphertext": ciphertext.hex(),
-            "steps": aes.logs
-        })
+        return jsonify({"ciphertext": ciphertext.hex(), "steps": aes.logs})
     except Exception as e:
-        return jsonify({"error": str(e)}), 500 
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/decrypt', methods=['POST'])
+
+@app.route("/decrypt", methods=["POST"])
 def decrypt():
     try:
         data = request.get_json()
-        key, ciphertext = parse_input(data, 'key', 'ciphertext')
+        key, ciphertext = parse_input(data, "key", "ciphertext")
 
-        if(len(key) % 16 != 0):
+        if len(key) % 16 != 0:
             key = pad(key)
 
         aes = AES(key)
         plaintext = aes.decrypt_block(ciphertext)
 
-        return jsonify({
-            "plaintext": plaintext.decode(errors='replace'),
-            "steps": aes.logs
-        })
+        return jsonify(
+            {"plaintext": plaintext.decode(errors="replace"), "steps": aes.logs}
+        )
     except Exception as e:
         return jsonify({"message": str(e)}), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
